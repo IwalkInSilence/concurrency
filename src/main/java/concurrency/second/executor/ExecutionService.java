dd5232a8d94284714451f0aa2404f1bda3465985
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2021-02-25 18:55:35 +0200

    fix mvn

--- src/main/java/concurrency/second/executor/ExecutionService.java
+++ src/main/java/concurrency/second/executor/ExecutionService.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
 import org.apache.commons.collections4.ListUtils;
 
 public class ExecutionService {
@@ -36,7 +35,7 @@ public class ExecutionService {
                 result += future.get();
             }
         } catch (InterruptedException | ExecutionException e) {
-           throw new RuntimeException("ExecutionService don't work correct", e);
+            throw new RuntimeException("ExecutionService don't work correct", e);
         }
         executorService.shutdownNow();
         return result;
